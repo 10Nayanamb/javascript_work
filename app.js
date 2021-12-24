9ff332736b8b97f7d8480dd1bc5a7b714cf3b815
10Nayanamb <88362758+10Nayanamb@users.noreply.github.com>
2021-12-24 16:51:16 +0530

    multiplayer functionality added

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
-var totalMoney=parseInt(prompt("How much money do you have?"));
 const button1=document.getElementById('button1');
-const sitton=document.getElementById('Sitton');
-const munda=document.getElementById('Munda');
-const iska=document.getElementById('Iska');
-const rohi=document.getElementById('Rohi');
-const dhandi=document.getElementById('Dhandi');
-const hotton=document.getElementById('Hotton');
-var idArray=[sitton,munda,iska,rohi,dhandi,hotton];
 button1.addEventListener('click',()=>{
+  const sitton=document.getElementById('Sitton').value;
+  const munda=document.getElementById('Munda').value;
+  const iska=document.getElementById('Iska').value;
+  const rohi=document.getElementById('Rohi').value;
+  const dhandi=document.getElementById('Dhandi').value;
+  const hotton=document.getElementById('Hotton').value;
+  var idArray=[sitton,munda,iska,rohi,dhandi,hotton];
   noArray=[];
   symbolArray=[];
   for (let i=0;i<6;i++){
@@ -23,45 +22,54 @@ button1.addEventListener('click',()=>{
   document.getElementById('image4').innerHTML=`<img src="image/${symbolArray[3]}.png"/>`;
   document.getElementById('image5').innerHTML=`<img src="image/${symbolArray[4]}.png"/>`;
   document.getElementById('image6').innerHTML=`<img src="image/${symbolArray[5]}.png"/>`;
-  var choice=getSignFromId(idArray);
-  var money=parseInt(getMoneyFromId(idArray));
-  const count=elCounter(symbolArray,choice);
-  if (totalMoney<=0){
-    document.querySelector('h3').textContent=`Oops you have lost all your money!`;
+  var FlagNo = getOccurrence(symbolArray, 'Flag')
+  var MaskNo = getOccurrence(symbolArray, 'Mask')
+  var HeartNo = getOccurrence(symbolArray, 'Heart')
+  var SpadeNo = getOccurrence(symbolArray, 'Spade')
+  var DiamondNo = getOccurrence(symbolArray, 'Diamond')
+  var ClubNo = getOccurrence(symbolArray, 'Club')
+  var resultString = "";
+  if (FlagNo>1){
+    resultString += ` Flag: ${FlagNo}` 
   }
-  else if (totalMoney<money){
-    document.querySelector('h3').textContent=`Oops you dont have that much money!`;
+  if (MaskNo>1){
+    resultString += ` Mask: ${MaskNo}` 
   }
-  else if (count>1){
-    moneyWon=money*count;
-    totalMoney=totalMoney+moneyWon;
-    document.querySelector('h3').textContent=`Congrats You Won ${moneyWon}, you have ${totalMoney} now`;
-  }else {
-    totalMoney=totalMoney-money;
-    document.querySelector('h3').textContent=`Oops You lost ${money}, you have ${totalMoney} now!`;
+  if (HeartNo>1){
+    resultString += ` Heart: ${HeartNo}` 
   }
+  if (SpadeNo>1){
+    resultString += ` Spade: ${SpadeNo}` 
+  }
+  if (DiamondNo>1){
+    resultString += ` Diamond: ${DiamondNo}` 
+  }
+  if (ClubNo>1){
+    resultString += ` Club: ${ClubNo}` 
+  }
+  document.querySelector('h3').textContent=resultString;
 });
 });
 
 function roll(no){
   switch (no) {
     case 1:
-      return "Dhandi";
+      return "Flag";
       break;
     case 2:
-      return "Munda";
+      return "Mask";
       break;
     case 3:
-      return "Iska";
+      return "Spade";
       break;
     case 4:
-      return "Hotton";
+      return "Heart";
       break;
     case 5:
-      return "Sitton";
+      return "Club";
       break;
     default:
-      return "Rohi";
+      return "Diamond";
   }
 }
 
@@ -73,25 +81,8 @@ function getRandomNo(){
   return Math.floor(Math.random()*6+1);
 }
 
-function getSignFromId(idArray){
-  var signChosen="invalid";
-  for (let i of idArray){
-    if (i.value!=0){
-      signChosen=i.name;
-      break;
-    }
-  }
-  return signChosen;
-}
-
-function getMoneyFromId(idArray){
-  var moneyChosen=0;
-  for (let i of idArray){
-    if (i.value>0){
-      moneyChosen=i.value;
-      i.value="0";
-      break;
-    }
-  }
-  return moneyChosen;
+function getOccurrence(array, value) {
+  var count = 0;
+  array.forEach((v) => (v === value && count++));
+  return count;
 }
